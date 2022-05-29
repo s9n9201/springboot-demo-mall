da7000c3a71ac28b4838719607149dc5d9e3b97d
kent <s9n9201@yahoo.com.tw>
2022-05-29 23:15:48 +0800

    add search method

--- src/main/java/webkent/demo/springbootmall/Repository/ProductRepository.java
+++ src/main/java/webkent/demo/springbootmall/Repository/ProductRepository.java
@@ -3,5 +3,8 @@ package webkent.demo.springbootmall.Repository;
 import org.springframework.data.repository.CrudRepository;
 import webkent.demo.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Product, Integer> {
+    List<Product> findByProductName(String productName);
 }
