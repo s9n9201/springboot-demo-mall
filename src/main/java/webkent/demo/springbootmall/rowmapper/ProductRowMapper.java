755206ce7f3b961793a5b71fcbc277c43a50873d
kent <s9n9201@yahoo.com.tw>
2022-05-29 23:06:35 +0800

    finished

--- src/main/java/webkent/demo/springbootmall/rowmapper/ProductRowMapper.java
+++ src/main/java/webkent/demo/springbootmall/rowmapper/ProductRowMapper.java
@@ -10,7 +10,14 @@ public class ProductRowMapper implements RowMapper<Product> {
     @Override
     public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
         Product product=new Product();
-
-        return null;
+        product.setProductName(rs.getString("product_name"));
+        product.setCategory(rs.getString("category"));
+        product.setImageUrl(rs.getString("image_url"));
+        product.setPrice(rs.getInt("price"));
+        product.setStock(rs.getInt("stock"));
+        product.setDescription(rs.getString("description"));
+        product.setCreateDate(rs.getTimestamp("created_date"));
+        product.setLastModifiedDate(rs.getTimestamp("last_modified_date"));
+        return product;
     }
 }
