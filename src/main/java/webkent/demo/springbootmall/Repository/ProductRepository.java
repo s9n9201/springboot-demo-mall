7815765370d1b05353200abb881b49d561c39994
kent <s9n9201@yahoo.com.tw>
2022-05-30 22:00:35 +0800

    add getProduct method

--- src/main/java/webkent/demo/springbootmall/Repository/ProductRepository.java
+++ src/main/java/webkent/demo/springbootmall/Repository/ProductRepository.java
@@ -3,8 +3,11 @@ package webkent.demo.springbootmall.Repository;
 import org.springframework.data.repository.CrudRepository;
 import webkent.demo.springbootmall.model.Product;
 
+
 import java.util.List;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
     List<Product> findByProductName(String productName);
+
+    Product getProductByProductId(Integer prodcutId);
 }
